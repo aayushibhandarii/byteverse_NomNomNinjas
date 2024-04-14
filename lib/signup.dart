ec526774511b51205b0d06c7cd4fec66674403d1
Palak Jaiswal <143824974+Palak24Ol@users.noreply.github.com>
2024-04-14 16:21:23 +0530

    Update signup

--- lib/signup.dart
+++ lib/signup.dart
@@ -24,7 +24,7 @@ class _signupPageState extends State<signupPage> {
         UserCredential? usercredential;         //  ? means it can be null
         try{
           usercredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password).then((value){       // .then is used to tell what to do user created the account
-            Navigator.pushNamed(context, '/homepg');
+            Navigator.pushNamed(context, '/location');
           });    // as we are handling with future values hence we use async and await     // if here values are putted by the user then create a new user
           }
         on FirebaseAuthException catch(e){        // to handle the exception in try   //ex: if user created another account with same email and password or network issue while entering email or password
