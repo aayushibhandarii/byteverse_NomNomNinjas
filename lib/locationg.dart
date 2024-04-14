97d557207c9abc04132f7f4b7815ff2fb6ddef80
Palak Jaiswal <143824974+Palak24Ol@users.noreply.github.com>
2024-04-14 17:41:21 +0530

    Update locationg

--- lib/locationg.dart
+++ lib/locationg.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
-class MapLocationPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map Location'),
-      ),
-      body: const Center(
-        child: Text('Select location from maps here'),
-      ),
-    );
-  }
-
-}
+//
